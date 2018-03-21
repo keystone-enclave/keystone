18fe55e310e5388fb6796fb010e4848e773b16e6
Dayeol Lee <dayeol@berkeley.edu>
2018-03-21 20:48:40 +0000

    run-qemu script

--- run-qemu.sh
+++ run-qemu.sh
@@ -2,5 +2,5 @@ sudo ./qemu-system-riscv64 -D debug.log -nographic -machine virt \
   -kernel bbl -append "root=/dev/vda ro console=ttyS0" \
   -drive file=busybear.bin,format=raw,id=hd0 \
   -device virtio-blk-device,drive=hd0 \
-  -netdev type=tap,script=./ifup,downscript=./ifdown,id=net0 \
+  #-netdev type=tap,script=./ifup,downscript=./ifdown,id=net0 \
   -device virtio-net-device,netdev=net0
