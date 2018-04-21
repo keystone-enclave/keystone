99c81d7d1ec3b9f4e9cbed74c4a512468a5a945c
Dayeol Lee <dayeol@berkeley.edu>
2018-04-21 01:48:09 +0000

    Update submodules/ run-qemu script

--- run-qemu-gdb.sh
+++ run-qemu-gdb.sh
@@ -1,4 +1,4 @@
-./qemu-system-riscv64 -D debug.log -nographic -machine virt \
+gdb --args ./qemu-system-riscv64 -d in_asm,out_asm,int,mmu,exec,cpu -D debug.log -nographic -machine virt \
 	-secure-boot sanctum_bootloader/boot_trng.bin \
   -kernel bbl -append "root=/dev/vda ro console=ttyS0" \
   -drive file=busybear.bin,format=raw,id=hd0 \
