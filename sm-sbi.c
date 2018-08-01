421e11d1d516954f1c040a4d68535daf94ae571e
Dayeol Lee <dayeol@berkeley.edu>
2018-08-01 20:09:36 +0000

    identify host satp

--- sm-sbi.c
+++ sm-sbi.c
@@ -1,31 +1,30 @@
 #include "sm-sbi.h"
 #include "pmp.h"
 #include "enclave.h"
+#include <errno.h>
 
 int mcall_sm_create_enclave(unsigned long base, unsigned long size)
 {
-  unsigned long ptbr = read_csr(satp);
-  printm("ptbr: 0x%llx\n", ptbr);
   return create_enclave((uintptr_t) base, (uintptr_t) size);
 }
 
 int mcall_sm_destroy_enclave(unsigned long eid)
 {
-  unsigned long ptbr = read_csr(satp);
-  printm("ptbr: 0x%llx\n", ptbr);
+  if(get_host_satp(eid) != read_csr(satp))
+    return -EFAULT;
   return destroy_enclave((int)eid);
 }
 
-int mcall_sm_copy_from_enclave(unsigned long ptr, unsigned long size)
+int mcall_sm_copy_from_enclave(unsigned long eid, unsigned long ptr, unsigned long size)
 {
-  unsigned long ptbr = read_csr(satp);
-  printm("ptbr: 0x%llx\n", ptbr);
-  return copy_from_enclave(0, (void*) ptr, (size_t) size);
+  if(get_host_satp(eid) != read_csr(satp))
+    return -EFAULT;
+  return copy_from_enclave(eid, (void*) ptr, (size_t) size);
 }
 
-int mcall_sm_copy_to_enclave(unsigned long ptr, unsigned long size)
+int mcall_sm_copy_to_enclave(unsigned long eid, unsigned long ptr, unsigned long size)
 {
-  unsigned long ptbr = read_csr(satp);
-  printm("ptbr: 0x%llx\n", ptbr);
-  return copy_to_enclave(0, (void*) ptr, (size_t) size);
+  if(get_host_satp(eid) != read_csr(satp))
+    return -EFAULT;
+  return copy_to_enclave(eid, (void*) ptr, (size_t) size);
 }
