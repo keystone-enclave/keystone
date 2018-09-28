bc578f95f41d021e6fcad61a86c0b01941f0772f
Dayeol Lee <dayeol@berkeley.edu>
2018-09-28 11:54:32 -0700

    Two changes in the repo (#3)
    * Bumped QEMU
    
    * Separated runtime from sdk, moved scripts to a directory

--- copy_sdk_bin.sh
+++
@@ -1,6 +0,0 @@
-echo "Copying binary $1"
-mkdir -p /tmp/busybear
-sudo mount busybear-linux/busybear.bin /tmp/busybear
-sudo cp sdk/$1 /tmp/busybear/root
-sudo umount /tmp/busybear
-rmdir /tmp/busybear
