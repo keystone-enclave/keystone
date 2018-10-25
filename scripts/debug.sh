4299f18bc09888cf1985bd1b330bbd29d596b1a9
Kevin Läufer <laeufer@cs.berkeley.edu>
2018-10-24 17:12:43 -0700

    [scripts] specify bash as program loader
    https://en.wikipedia.org/wiki/Shebang_(Unix)

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #./riscv-qemu/riscv64-softmmu/qemu-system-riscv64 -d in_asm \ #in_asm,out_asm,int,mmu,exec,cpu
 #gdb --args ./riscv-qemu/riscv64-softmmu/qemu-system-riscv64 -d in_asm -D debug.log -nographic -machine virt \
 ./riscv-qemu/riscv64-softmmu/qemu-system-riscv64 \
