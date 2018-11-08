e1f5d2cb9e3a1681f37bfec9efaa0ed6e1e59802
Dayeol Lee <dayeol@berkeley.edu>
2018-11-07 16:11:51 -0800

    Getting entry point through a1 instead of a0 (#1)

--- runtime/page_fault.c
+++ runtime/page_fault.c
@@ -12,6 +12,6 @@ void rt_page_fault(struct encl_ctx_t* ctx)
   cause = ctx->scause;
   
   printf("[runtime] page fault at 0x%lx (scause: 0x%lx)\n", addr, cause);
-  
+  SBI_CALL_1(SBI_SM_EXIT_ENCLAVE,-1);
   return;
 }
