572ec3580cae57aad98b7724dbdb098e7b133a82
Dayeol Lee <dayeol@berkeley.edu>
2018-11-15 05:03:03 +0000

    [Doc] Initial Documentation with Read the Docs

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# Building Docs
+
+```
+sudo pip install -r requirements.txt
+make html
+```
+
+# Local Hosting
+
+You can locally host the built doc by running
+
+```
+python -m SimpleHTTPServer
+```
+
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = u'Keystone Enclave'
-copyright = u'2018, Dayeol Lee, David Kohlbrenner, Dawn Song, Krste Asanovic'
-author = u'Dayeol Lee, David Kohlbrenner, Dawn Song, Krste Asanovic'
+copyright = u'2018, Dayeol Lee, and David Kohlbrenner'
+author = u'Dayeol Lee, and David Kohlbrenner'
 
 # The short X.Y version
-version = u''
+version = u'0.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.1'
+release = u'0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -74,7 +74,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
