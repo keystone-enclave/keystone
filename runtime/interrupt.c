26d9df115af325407dc2624e8f5511eff2fdefbc
Dayeol Lee <dayeol@berkeley.edu>
2018-11-28 01:17:45 +0000

    Exit the enclave with error message when it gets unhandlable interrupt

--- runtime/interrupt.c
+++ runtime/interrupt.c
@@ -31,6 +31,8 @@ void handle_interrupts(struct encl_ctx_t* regs)
     case INTERRUPT_CAUSE_SOFTWARE:
     case INTERRUPT_CAUSE_EXTERNAL:
     default:
+      printf("[FATAL] Eyrie runtime could not handle the interrupt: 0x%lx\n", cause);
+      SBI_CALL_1(SBI_SM_EXIT_ENCLAVE,-1);
       return;
   }
 }
