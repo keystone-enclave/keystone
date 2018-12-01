7c9fee359d97f1a8027f88fff7be70e75b5a026e
David Kohlbrenner <dkohlbre@cs.ucsd.edu>
2018-11-30 20:57:06 -0800

    Changed bootrom to use hardcoded device keys for testing purposes. Bumped sdk, pk, and linux for compatability

---
+++ bootrom/use_test_keys.h
@@ -0,0 +1,3 @@
+#include "test_dev_key.h"
+memcpy(sanctum_dev_secret_key, _sanctum_dev_secret_key, _sanctum_dev_secret_key_len);
+memcpy(sanctum_dev_public_key, _sanctum_dev_public_key, _sanctum_dev_public_key_len);
