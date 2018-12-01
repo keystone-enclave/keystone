6c6ab82e87e82b61d9de836acff4cdcfcf4de851
David Kohlbrenner <dkohlbre@cs.ucsd.edu>
2018-11-30 20:57:06 -0800

    Changed bootrom to use hardcoded device keys for testing purposes. Bumped sdk, pk, and linux for compatability

--- bootrom/bootloader.c
+++ bootrom/bootloader.c
@@ -39,8 +39,10 @@ extern byte sanctum_sm_secret_key[64];
 extern byte sanctum_sm_signature[64];
 #define DRAM_BASE 0x80000000
 
-inline byte random_byte() {
-	return 0xda;
+/* Update this to generate valid entropy for target platform*/
+inline byte random_byte(unsigned int i) {
+#warning Bootloader does not have entropy source, keys are for TESTING ONLY
+  return 0xac + (0xdd ^ i);
 }
 
 void bootloader() {
@@ -54,13 +56,27 @@ void bootloader() {
   // sd_init();
   // sd_read_from_start(DRAM, 1024);
 
-  // Create a random seed for a device key from TRNG
+  /* Gathering high quality entropy during boot on embedded devices is
+   * a hard problem. Platforms taking security seriously must provide
+   * a high quality entropy source available in hardware. Platforms
+   * that do not provide such a source must gather their own
+   * entropy. See the Keystone documentation for further
+   * discussion. For testing purposes, we have no entropy generation.
+  */
+
+  // Create a random seed for keys and nonces from TRNG
   for (unsigned int i=0; i<32; i++) {
-    scratchpad[i] = 0xac + (0xdd ^ i);//random_byte();
+    scratchpad[i] = random_byte(i);
   }
 
+  /* On a real device, the platform must provide a secure root device
+     keystore. For testing purposes we hardcode a known private/public
+     keypair */
+  // TEST Device key
+  #include "use_test_keys.h"
+  
   // Derive {SK_D, PK_D} (device keys) from a 32 B random seed
-  ed25519_create_keypair(sanctum_dev_public_key, sanctum_dev_secret_key, scratchpad);
+  //ed25519_create_keypair(sanctum_dev_public_key, sanctum_dev_secret_key, scratchpad);
 
   // Measure SM
   sha3_init(&hash_ctx, 64);
