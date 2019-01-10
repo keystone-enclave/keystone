83c7b82b5d70d92f4767b1228ab035df27471f78
Dayeol Lee <dayeol@berkeley.edu>
2019-01-10 00:09:59 +0000

    [Documentation] Modified conf.py to make readthedocs.org parse *.md

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,8 +38,11 @@ release = u'0.1.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {'.md': CommonMarkParser}
+
+source_suffix = ['.rst', '.md']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
