5ca27fa0d547c659093c5a84aefefdb6e05331b7
Dayeol Lee <dayeol@berkeley.edu>
2019-01-29 19:16:37 +0000

    Enable CMA in the kernel and the driver
    Keystone driver will try to allocate from contiguous CMA memory
    instead of buddy allocation system.
    This will allow the driver to successfully allocate a larger enclave
    memory which could be several gigabytes.

--- scripts/apply-patch.sh
+++ scripts/apply-patch.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 cd riscv-qemu
-patch hw/riscv/boot.c < ../patches/qemu-kernel-reloc.patch
+patch --forward hw/riscv/boot.c < ../patches/qemu-kernel-reloc.patch || true
+cd ..
+
+# linux patch
+cd riscv-linux
+patch -p0 --forward <  ../patches/linux-cma.patch || true
+cd ..
