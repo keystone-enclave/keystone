54b3bec2b6b2c79d4c5a72ec621873dbfff4ba12
Dayeol Lee <dayeol@berkeley.edu>
2019-01-31 00:54:17 +0000

    Final Final please

--- fast-setup.sh
+++ fast-setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+echo "Starting..."
+if ( $(command -v riscv64-unknown-linux-gnu-gcc > /dev/null) && 
+  $(command -v riscv64-unknown-elf-gcc > /dev/null) )
+then
+  echo "RISCV tools are already installed"
+else
+  echo "Downloading Prebuilt RISC-V Toolchain... "
+  export RISCV=$(pwd)/riscv
+  export PATH=$PATH:$RISCV/bin
+  git clone https://github.com/keystone-enclave/firesim-riscv-tools-prebuilt.git --depth 1
+  cd firesim-riscv-tools-prebuilt
+  ./installrelease.sh > riscv-tools-install.log 
+  mv distrib ../riscv
+  cd ..
+fi
+
 git config --global submodule.riscv-gnu-toolchain.update none
 git submodule update --init --recursive
-
-git clone https://github.com/keystone-enclave/firesim-riscv-tools-prebuilt.git --depth 1
-export RISCV=$(pwd)/riscv
-export PATH=$PATH:$RISCV/bin
-
-cd firesim-riscv-tools-prebuilt
-./installrelease.sh > riscv-tools-install.log 
-mv distrib ../riscv
-cd ..
