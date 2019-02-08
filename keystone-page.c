ef5a916cf452fbc0d700056c5b6e895cf23b40e0
David Kohlbrenner <dkohlbre@berkeley.edu>
2019-02-08 11:24:10 -0800

    Added dirty bit to all pages marked W, required for hw

--- keystone-page.c
+++ keystone-page.c
@@ -228,7 +228,7 @@ vaddr_t epm_alloc_rt_page_noexec(epm_t* epm, vaddr_t addr)
 
 vaddr_t epm_alloc_rt_page(epm_t* epm, vaddr_t addr)
 {
-  return epm_alloc_page(epm, addr, PTE_A | PTE_R | PTE_W | PTE_X);
+  return epm_alloc_page(epm, addr, PTE_D | PTE_A | PTE_R | PTE_W | PTE_X);
 }
 
 vaddr_t epm_alloc_user_page_noexec(epm_t* epm, vaddr_t addr)
@@ -238,7 +238,7 @@ vaddr_t epm_alloc_user_page_noexec(epm_t* epm, vaddr_t addr)
 
 vaddr_t epm_alloc_user_page(epm_t* epm, vaddr_t addr)
 {
-  return epm_alloc_page(epm, addr, PTE_A | PTE_R | PTE_X | PTE_W | PTE_U);
+  return epm_alloc_page(epm, addr, PTE_D | PTE_A | PTE_R | PTE_X | PTE_W | PTE_U);
 }
 
 void epm_free_page(epm_t* epm, vaddr_t addr)
