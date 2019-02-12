534adf2acef38babf6714c0aa458e4fcba743633
dkohlbre <dkohlbre@cs.ucsd.edu>
2019-02-11 19:57:50 -0800

    Updated fast start script to use tarball, added hifive build to new stage

--- fast-setup.sh
+++ fast-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting..."
-if ( $(command -v riscv64-unknown-linux-gnu-gcc > /dev/null) && 
+if ( $(command -v riscv64-unknown-linux-gnu-gcc > /dev/null) &&
   $(command -v riscv64-unknown-elf-gcc > /dev/null) )
 then
   echo "RISCV tools are already installed"
@@ -9,11 +9,11 @@ else
   echo "Downloading Prebuilt RISC-V Toolchain... "
   export RISCV=$(pwd)/riscv
   export PATH=$PATH:$RISCV/bin
-  git clone https://github.com/keystone-enclave/firesim-riscv-tools-prebuilt.git --depth 1
-  
-  cd firesim-riscv-tools-prebuilt
-  ./installrelease.sh > riscv-tools-install.log 
-  mv distrib riscv  
+  wget https://github.com/keystone-enclave/firesim-riscv-tools-prebuilt/archive/1.0.tar.gz
+  tar -xzvf 1.0.tar.gz
+  cd firesim-riscv-tools-prebuilt-1.0
+  ./installrelease.sh > riscv-tools-install.log
+  mv distrib riscv
   cp -R riscv ../
   cd ..
   echo "Toolchain has been installed in $RISCV"
