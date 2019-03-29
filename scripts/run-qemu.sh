54114395b22465ef6bb3ff3c149658cfbbdaa53b
Dayeol Lee <dayeol@berkeley.edu>
2019-03-29 04:48:18 +0000

    bump SDK and modified script/run-qemu.sh

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -4,8 +4,17 @@ HOST_PORT=${HOST_PORT:="$((3000 + RANDOM % 3000))"}
 
 echo "**** Running QEMU SSH on port ${HOST_PORT} ****\n"
 
+DEBUG=""
+
+while [ "$1" != "" ]; do
+if [ "$1" = "-debug" ]; then
+  DEBUG="-s -S -d in_asm -D debug.log"
+  shift
+fi
+done
+
 ./riscv-qemu/riscv64-softmmu/qemu-system-riscv64 \
-    -D debug.log \
+    $DEBUG \
     -m 4G \
     -bios bootrom/bootrom.elf \
     -nographic \
