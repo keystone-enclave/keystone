a93b91ed9d25e569923b5c5a37ef06c9488d5185
Dayeol Lee <dayeol@berkeley.edu>
2019-07-17 21:57:01 +0000

    Update document version to 0.3
    This doesn't usually matter, but will be shown in the pdf version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018, Dayeol Lee, and David Kohlbrenner'
 author = u'Dayeol Lee, and David Kohlbrenner'
 
 # The short X.Y version
-version = u'0.1'
+version = u'0.3'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.1'
+release = u'0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
