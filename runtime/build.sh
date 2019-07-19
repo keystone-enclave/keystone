2d5a37be1bf3bd040cbda93fd66008d6194e5abf
dkohlbre <dkohlbre@cs.ucsd.edu>
2019-07-19 13:40:34 -0700

    Simplified buildscript to use maps

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
 EYRIE_SOURCE_DIR=`dirname $0`
-PLUGINS=${@:1}
+REQ_PLUGINS=${@:1}
+
+# Build known plugins
+declare -A PLUGINS
+PLUGINS[freemem]="-DUSE_FREEMEM "
+PLUGINS[untrusted_io_syscall]="-DIO_SYSCALL_WRAPPING "
+PLUGINS[linux_syscall]="-DLINUX_SYSCALL_WRAPPING "
+PLUGINS[env_setup]="-DENV_SETUP "
+PLUGINS[strace_debug]="-DINTERNAL_STRACE "
+#PLUGINS[dynamic_resizing]="-DDYN_ALLOCATION "
 
 OPTIONS_FLAGS=
 
-# OPTIONS_FLAGS ?= -DIO_SYSCALL_WRAPPING -DLINUX_SYSCALL_WRAPPING -DUSE_FREEMEM -DENV_SETUP
-# OPTIONS_FLAGS += -DDYN_ALLOCATION
-# OPTIONS_FLAGS += -DINTERNAL_STRACE
-
-for plugin in $PLUGINS; do
-  case $plugin in
-    freemem)
-      OPTIONS_FLAGS+="-DUSE_FREEMEM "
-      ;;
-    untrusted_io_syscall)
-      OPTIONS_FLAGS+="-DIO_SYSCALL_WRAPPING "
-      ;;
-    linux_syscall)
-      OPTIONS_FLAGS+="-DLINUX_SYSCALL_WRAPPING "
-      ;;
-    env_setup)
-      OPTIONS_FLAGS+="-DENV_SETUP "
-      ;;
-    *)
-      echo "Unknown plugin '$plugin'. Skipping"
-      ;;
-  esac
+for plugin in $REQ_PLUGINS; do
+    if [[ ! ${PLUGINS[$plugin]+_} ]]; then
+        echo "Unknown Eyrie plugin '$plugin'. Skipping"
+    else
+        OPTIONS_FLAGS+=${PLUGINS[$plugin]}
+    fi
 done
 
 export OPTIONS_FLAGS
