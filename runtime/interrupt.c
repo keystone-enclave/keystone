1be9bad6e928973fd57dbe286092679a0e1e6329
Dayeol Lee <dayeol@berkeley.edu>
2019-09-10 06:19:10 +0000

    Timer interrupt bug
    Refer to https://github.com/keystone-enclave/keystone/issues/113
    Timer interrupt has been disabled, so the enclave never yields the CPU

--- runtime/interrupt.c
+++ runtime/interrupt.c
@@ -7,12 +7,15 @@
 #include "timex.h"
 #include "interrupt.h"
 #include "printf.h"
+#include <asm/csr.h>
 
-#define DEFAULT_CLOCK_DELAY 40000
+#define DEFAULT_CLOCK_DELAY 10000
 
 void init_timer(void)
 {
   sbi_set_timer(get_cycles64() + DEFAULT_CLOCK_DELAY);
+  csr_set(sstatus, SR_SPIE);
+  csr_set(sie, SIE_STIE | SIE_SSIE);
 }
 
 void handle_timer_interrupt()
@@ -20,6 +23,7 @@ void handle_timer_interrupt()
   sbi_stop_enclave(0);
   unsigned long next_cycle = get_cycles64() + DEFAULT_CLOCK_DELAY;
   sbi_set_timer(next_cycle);
+  csr_set(sstatus, SR_SPIE);
   return;
 }
 
