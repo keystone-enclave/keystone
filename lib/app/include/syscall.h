03166fbd78773239a83c0388963013de508bf0e0
Kopf, Benedikt <benedikt.kopf@aisec.fraunhofer.de>
2020-04-22 10:38:57 +0200

    Add sealing key feature

--- lib/app/include/syscall.h
+++ lib/app/include/syscall.h
@@ -7,11 +7,13 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include "sealing.h"
 /* TODO We should be syncing these more explictly with the runtime
    defs */
 #define SYSCALL_OCALL 1001
 #define SYSCALL_SHAREDCOPY  1002
 #define SYSCALL_ATTEST_ENCLAVE  1003
+#define SYSCALL_GET_SEALING_KEY 1004
 #define SYSCALL_EXIT  1101
 
 #define SYSCALL(which, arg0, arg1, arg2, arg3, arg4) ( {	\
@@ -32,6 +34,7 @@
 #define SYSCALL_1(which, arg0) SYSCALL(which, arg0, 0, 0, 0, 0)
 #define SYSCALL_2(which, arg0, arg1) SYSCALL(which, arg0, arg1, 0, 0, 0)
 #define SYSCALL_3(which, arg0, arg1, arg2) SYSCALL(which, arg0, arg1, arg2, 0, 0)
+#define SYSCALL_4(which, arg0, arg1, arg2, arg3) SYSCALL(which, arg0, arg1, arg2, arg3, 0)
 #define SYSCALL_5(which, arg0, arg1, arg2, arg3, arg4) SYSCALL(which, arg0, arg1, arg2, arg3, arg4)
 
 int copy_from_shared(void* dst,
@@ -42,5 +45,9 @@ int ocall(unsigned long call_id,
 	  void* return_buffer, size_t return_len);
 uintptr_t untrusted_mmap();
 int attest_enclave(void* report, void* data, size_t size);
+
+int get_sealing_key(struct sealing_key *sealing_key_struct,
+                    size_t sealing_key_struct_size,
+                    void *key_ident, size_t key_ident_size);
 
 #endif /* syscall.h */
