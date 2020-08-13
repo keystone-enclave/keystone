718da043b11ea1c762526315743e7b22aac1d950
Alex Thomas <alexthomas@berkeley.edu>
2020-08-13 22:18:50 +0000

    Expand message test

--- tests/tests/message/message.c
+++ tests/tests/message/message.c
@@ -1,7 +1,28 @@
 #include "eapp_utils.h"
 #include "syscall.h"
+#include "edge_wrapper.h"
 
 void EAPP_ENTRY eapp_entry(){
-   char* msg = "hello world!\n";
-   send_msg(1, msg, 13);  
+   char* msg1 = "MESSAGE 1!\n";
+   char *msg2 = "MESSAGE 2!\n"; 
+   char *msg3 = "MESSAGE 3!\n"; 
+
+   char buf[13]; 
+   int uid = 0; 
+   edge_init(); 
+
+   send_msg(uid, msg1, 13);
+   send_msg(uid, msg2, 13);
+   send_msg(uid, msg3, 13);
+
+   recv_msg(uid, buf, 13);
+   ocall_print_buffer(buf, 13);
+   
+   recv_msg(uid, buf, 13);
+   ocall_print_buffer(buf, 13);
+
+   recv_msg(uid, buf, 13);
+   ocall_print_buffer(buf, 13);
+
+   EAPP_RETURN(0); 
 }
