827e70388b7ac7ea7d77ccb2b404f4e3f993dab0
Gui Andrade <gh@archshift.com>
2020-09-15 17:14:52 -0700

    Integrate merkle tree into encrypted paging system

--- runtime/paging.c
+++ runtime/paging.c
@@ -6,13 +6,22 @@
 
 #include "paging.h"
 
+#include <stdatomic.h>
+
+#include "aes.h"
+#include "merkle.h"
+#include "sha256.h"
 #include "vm.h"
 
 uintptr_t paging_backing_storage_addr;
 uintptr_t paging_backing_storage_size;
 uintptr_t paging_next_backing_page_offset;
+uintptr_t paging_inc_backing_page_offset_by;
 
 static uintptr_t paging_user_page_count = 0;
+
+#define NUM_CTR_INDIRECTS 24
+static uintptr_t ctr_indirect_ptrs[NUM_CTR_INDIRECTS];
 
 extern uintptr_t rt_trap_table;
 
@@ -29,19 +38,22 @@ void paging_dec_user_page(void)
 
 uintptr_t
 paging_alloc_backing_page() {
-  uintptr_t next_page;
+  uintptr_t offs_update =
+      (paging_next_backing_page_offset + paging_inc_backing_page_offset_by) %
+      paging_backing_storage_size;
 
   /* no backing page available */
-  if (paging_next_backing_page_offset >= paging_backing_storage_size) {
+  if (offs_update == 0) {
+    // cycled through all the pages
     warn("no backing page avaiable");
     return 0;
   }
 
-  next_page = paging_backing_storage_addr + paging_next_backing_page_offset;
+  uintptr_t next_page =
+      paging_backing_storage_addr + paging_next_backing_page_offset;
   assert(IS_ALIGNED(next_page, RISCV_PAGE_BITS));
 
-  paging_next_backing_page_offset += RISCV_PAGE_SIZE;
-
+  paging_next_backing_page_offset = offs_update;
   return next_page;
 }
 
@@ -49,6 +61,25 @@ unsigned int
 paging_remaining_pages()
 {
   return (paging_backing_storage_size - paging_next_backing_page_offset)/RISCV_PAGE_SIZE;
+}
+
+uintptr_t
+gcd(uintptr_t a, uintptr_t b) {
+  while (b) {
+    uintptr_t tmp = b;
+    b             = a % b;
+    a             = tmp;
+  }
+  return a;
+}
+
+uintptr_t
+find_coprime_of(uintptr_t n) {
+  uintptr_t res;
+  do {
+    res = n / 2 + sbi_random() % (n / 2);
+  } while (gcd(res, n) != 1);
+  return res;
 }
 
 void init_paging(uintptr_t user_pa_start, uintptr_t user_pa_end)
@@ -76,6 +107,13 @@ void init_paging(uintptr_t user_pa_start, uintptr_t user_pa_end)
   paging_pa_start = addr;
   paging_backing_storage_size = size;
   paging_backing_storage_addr = __paging_va(addr);
+
+  uintptr_t backing_pages = paging_backing_storage_size / RISCV_PAGE_SIZE;
+  uintptr_t inc           = find_coprime_of(backing_pages);
+
+  paging_inc_backing_page_offset_by = inc * RISCV_PAGE_SIZE;
+  warn("num_pages = %zx, pagesize_inc = %zx", backing_pages, inc);
+
   paging_next_backing_page_offset = 0;
 
   debug("BACK: 0x%lx-0x%lx (%u KB), va 0x%lx", addr, addr + size, size/1024, paging_backing_storage_addr);
@@ -190,6 +228,103 @@ uintptr_t __pick_page()
   return target;
 }
 
+#ifdef USE_PAGE_CRYPTO
+static volatile atomic_bool paging_boot_key_reserved = false;
+static volatile atomic_bool paging_boot_key_set      = false;
+static uint8_t paging_boot_key[32];
+
+static void
+establish_boot_key(void) {
+  uint8_t boot_key_tmp[32];
+
+  if (atomic_load(&paging_boot_key_set)) {
+    // Key already set
+    return;
+  }
+
+  rt_util_getrandom(boot_key_tmp, 32);
+
+  if (atomic_flag_test_and_set(&paging_boot_key_reserved)) {
+    // Lost the race; key already being set. Spin until finished.
+    while (!atomic_load(&paging_boot_key_set))
+      ;
+    return;
+  }
+
+  memcpy(paging_boot_key, boot_key_tmp, 32);
+  atomic_store(&paging_boot_key_set, true);
+}
+#endif  // USE_PAGE_CRYPTO
+
+static uint64_t*
+pageout_ctr_ptr(uintptr_t page) {
+  assert(page >= paging_backing_storage_addr);
+  size_t idx          = (page - paging_backing_storage_addr) >> RISCV_PAGE_BITS;
+  size_t indirect_idx = idx / (RISCV_PAGE_SIZE / 8);
+  size_t interior_idx = idx % (RISCV_PAGE_SIZE / 8);
+  assert(indirect_idx < NUM_CTR_INDIRECTS);
+
+  if (!ctr_indirect_ptrs[indirect_idx]) {
+    ctr_indirect_ptrs[indirect_idx] = paging_alloc_backing_page();
+    // Fill ptr pages with random values so our counters start unpredictable
+    rt_util_getrandom((void*)ctr_indirect_ptrs[indirect_idx], RISCV_PAGE_SIZE);
+  }
+
+  return (uint64_t*)(ctr_indirect_ptrs[indirect_idx]) + interior_idx;
+}
+
+#ifdef USE_PAGE_HASH
+static merkle_node_t paging_merk_root = {};
+#endif
+
+static void
+encrypt_page(const void* addr, void* dst, uint64_t pageout_ctr) {
+  size_t len = RISCV_PAGE_SIZE;
+
+#ifdef USE_PAGE_CRYPTO
+  establish_boot_key();
+  uint8_t iv[32] = {0};
+  WORD key_sched[80];
+  aes_key_setup(paging_boot_key, key_sched, 256);
+
+  memcpy(iv + 8, &pageout_ctr, 8);
+
+  aes_encrypt_ctr((uint8_t*)addr, len, (uint8_t*)dst, key_sched, 256, iv);
+#else
+  memcpy(dst, addr, len);
+#endif
+}
+
+static void
+decrypt_page(const void* addr, void* dst, uint64_t pageout_ctr) {
+  size_t len = RISCV_PAGE_SIZE;
+
+#ifdef USE_PAGE_CRYPTO
+  establish_boot_key();
+  uint8_t iv[32] = {0};
+  WORD key_sched[80];
+  aes_key_setup(paging_boot_key, key_sched, 256);
+
+  memcpy(iv + 8, &pageout_ctr, 8);
+
+  aes_decrypt_ctr((uint8_t*)addr, len, (uint8_t*)dst, key_sched, 256, iv);
+#else
+  memcpy(dst, addr, len);
+#endif
+}
+
+static void
+hash_page(uint8_t* hash, void* page_addr, uint64_t pageout_ctr) {
+#ifdef USE_PAGE_HASH
+  SHA256_CTX hasher;
+
+  sha256_init(&hasher);
+  sha256_update(&hasher, page_addr, RISCV_PAGE_SIZE);
+  sha256_update(&hasher, (uint8_t*)&pageout_ctr, sizeof(pageout_ctr));
+  sha256_final(&hasher, hash);
+#endif
+}
+
 /* evict a page from EPM and store it to the backing storage
  * back_page (PA1) <-- epm_page (PA2) <-- swap_page (PA1)
  * if swap_page is 0, no need to write epm_page
@@ -201,20 +336,36 @@ void __swap_epm_page(uintptr_t back_page, uintptr_t epm_page, uintptr_t swap_pag
   assert(back_page >= paging_backing_storage_addr);
   assert(back_page < paging_backing_storage_addr + paging_backing_storage_size);
 
-  /* not implemented */
-  assert(!encrypt);
-
   char buffer[RISCV_PAGE_SIZE] = {0,};
   if (swap_page) {
     assert(swap_page == back_page);
     memcpy(buffer, (void*)swap_page, RISCV_PAGE_SIZE);
   }
 
-  memcpy((void*)back_page, (void*)epm_page, RISCV_PAGE_SIZE);
+  uint64_t* pageout_ctr    = pageout_ctr_ptr(back_page);
+  uint64_t old_pageout_ctr = *pageout_ctr;
+  uint64_t new_pageout_ctr = old_pageout_ctr + 1;
+
+  uint8_t new_hash[32];
+  hash_page(new_hash, (void*)epm_page, new_pageout_ctr);
+  encrypt_page((void*)epm_page, (void*)back_page, new_pageout_ctr);
 
   if (swap_page) {
-    memcpy((void*)epm_page, buffer, RISCV_PAGE_SIZE);
+    uint8_t old_hash[32];
+    decrypt_page((void*)buffer, (void*)epm_page, old_pageout_ctr);
+    hash_page(old_hash, (void*)epm_page, old_pageout_ctr);
+
+#ifdef USE_PAGE_HASH
+    bool ok = merk_verify(&paging_merk_root, back_page, old_hash);
+    assert(ok);
+#endif
   }
+
+#ifdef USE_PAGE_HASH
+  merk_insert(&paging_merk_root, back_page, new_hash);
+#endif
+
+  *pageout_ctr = new_pageout_ctr;
 
   return;
 }
@@ -252,7 +403,7 @@ uintptr_t paging_evict_and_free_one(uintptr_t swap_va)
   assert(target_pte && (*target_pte & PTE_U));
 
   src_pa = pte_ppn(*target_pte) << RISCV_PAGE_BITS;
-  __swap_epm_page(dest_va, __va(src_pa), swap_va, false);
+  __swap_epm_page(dest_va, __va(src_pa), swap_va, true);
 
   /* invalidate target PTE */
   *target_pte = pte_create_invalid(ppn(__paging_pa(dest_va)),
