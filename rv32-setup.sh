25a1c27a45eb23a053d22a7ae363a7c4d6de168b
Alex Thomas <alexthomas@berkeley.edu>
2020-10-08 06:42:59 +0000

    Add rv32 hash and setup

---
+++ rv32-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting RV32 Instllation..."
+
+if ( $(command -v riscv32-unknown-linux-gnu-gcc > /dev/null) &&
+  $(command -v riscv32-unknown-elf-gcc > /dev/null) )
+then
+  echo "RISCV32 tools are already installed"
+else
+  echo "Downloading Prebuilt RISC-V 32 Toolchain... "
+
+  export RISCV=$(pwd)/riscv32
+  export PATH=$PATH:$RISCV/bin
+  wget https://keystone-enclave.eecs.berkeley.edu/files/rv32gc.tar.gz
+
+  # Check tool integrity
+  echo "Verifying prebuilt toolchain integrity..."
+  sha256sum -c .prebuilt_tools_shasums --status --ignore-missing
+  if [[ $? != 0 ]]
+  then
+      echo "Toolchain binary download incomplete or corrupted. You can build the toolchain locally or try again."
+      exit 1
+  fi
+
+  tar -xzvf rv32gc.tar.gz
+  mv riscv32gc riscv32
+  rm rv32gc.tar.gz
+  echo "Toolchain has been installed in $RISCV"
+fi
--- source.sh
+++ source.sh
@@ -1,3 +1,4 @@
 export RISCV=$(pwd)/riscv
-export PATH=$RISCV/bin:$PATH
+export RISCV32=$(pwd)/riscv32
+export PATH=$RISCV/bin:$RISCV32/bin:$PATH
 export KEYSTONE_SDK_DIR=$(pwd)/sdk/build
