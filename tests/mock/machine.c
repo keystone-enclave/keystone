670540c160092d0ebc7146d8f06ca713b7f9c451
Dayeol Lee <dayeol@berkeley.edu>
2020-12-14 23:01:18 +0000

    Update CMocka tests to work with OpenSBI

--- tests/mock/machine.c
+++ tests/mock/machine.c
@@ -1,26 +1,71 @@
 #include <stdint.h>
-
+#include <sbi/riscv_locks.h>
+#include <sbi/sbi_string.h>
+#include <sbi/sbi_ecall.h>
 extern void mock_assert(const int result, const char* const expression,
                             const char * const file, const int line);
 
-void __wrap_printm(const char* s, ...)
+void __wrap_sbi_printf(const char* s, ...)
 {
   return;
 }
 
-void __wrap_poweroff(uint16_t code)
+void __wrap_sbi_hart_hang(uint16_t code)
 {
   mock_assert(0, "poweroff", __FILE__, __LINE__);
+}
+
+void __wrap_spin_lock(spinlock_t* lock)
+{
+  return;
+}
+
+void __wrap_spin_unlock(spinlock_t* lock)
+{
+  return;
+}
+
+void __wrap_spin_trylock(spinlock_t* lock)
+{
+  return;
+}
+
+void* __wrap_sbi_memcpy(void *dest, const void *src, size_t count)
+{
+  char *temp1 = dest;
+  const char *temp2 = src;
+
+  while (count > 0) {
+    *temp1++ = *temp2++;
+    count --;
+  }
+
+  return dest;
+}
+
+void* __wrap_sbi_memset(void *s, int c, size_t count)
+{
+  char *temp = s;
+
+  while(count > 0) {
+    count--;
+    *temp++ = c;
+  }
+
+  return s;
 }
 
 void __wrap_trap_vector_enclave(){
   return;
 }
 
-void __wrap_trap_vector(){
-  return; 
-} 
+void __wrap__trap_handler(){
+  return;
+}
 
-uint64_t __wrap_mtime; 
-long __wrap_disabled_hart_mask;
-uint64_t __wrap_hart_mask;
+int __wrap_sbi_ecall_register_extension(struct sbi_ecall_extension *ext)
+{
+  return 0;
+}
+
+void* __wrap_ecall_keystone_enclave;
