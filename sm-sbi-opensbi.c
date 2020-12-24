12153a9ef077f03dc9845cba349604a833320c2e
Dayeol Lee <dayeol@berkeley.edu>
2020-12-24 01:36:51 +0000

    Remove unnecessary lines

--- sm-sbi-opensbi.c
+++ sm-sbi-opensbi.c
@@ -18,11 +18,6 @@ static int sbi_ecall_keystone_enclave_handler(unsigned long extid, unsigned long
                      unsigned long *args, unsigned long *out_val,
                      struct sbi_trap_info *out_trap)
 {
-  /* Note: regs is the sbi_trap.h format, which matches (... enough)
-     the thread.h format, which means we can 'just use it' for now.
-     This really needs to be handled far more cleanly. */
-  // uintptr_t funcid = regs->a6; /* This was a7 in the old interface */
-  // uintptr_t args[0] = regs->a0, args[1] = regs->a1, args[2] = regs->a2, args[3] = regs->a3;
   uintptr_t retval;
   switch(funcid){
   case SBI_SM_CREATE_ENCLAVE:
@@ -66,25 +61,6 @@ static int sbi_ecall_keystone_enclave_handler(unsigned long extid, unsigned long
   return retval;
 
 }
-/*
-void sm_ipi_process(){
-  sbi_printf("ipi got %lx\r\n",csr_read(mhartid));
-  handle_pmp_ipi();
-}
-
-int sm_sbi_send_ipi(uintptr_t recipient_mask){
-  struct sbi_scratch *scratch =  sbi_scratch_thishart_ptr();
-  struct sbi_trap_info uptrap;
-  uptrap.epc = 0;
-  uptrap.cause = 0;
-  uptrap.tval = 0;
-  struct sbi_tlb_info tlb_flush_info;
-  tlb_flush_info.start = 0;
-  tlb_flush_info.size = 0;
-  sbi_printf("senidng ipi %lx\r\n", recipient_mask);
-  return sbi_ipi_send_many(scratch, &uptrap, &recipient_mask,
-                              SBI_SM_EVENT, &tlb_flush_info);
-}*/
 
 struct sbi_ecall_extension ecall_keystone_enclave = {
   .extid_start = SBI_EXT_EXPERIMENTAL_KEYSTONE_ENCLAVE,
