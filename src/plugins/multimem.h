a9d06df37fd20a16c9a1708aaec595f21d7ca79c
Gui Andrade <gh@archshift.com>
2021-04-08 15:56:52 -0700

    Update multimem SBIs to not output value in retval

--- src/plugins/multimem.h
+++ src/plugins/multimem.h
@@ -7,6 +7,6 @@
 #define MULTIMEM_GET_OTHER_REGION_SIZE 0x1
 #define MULTIMEM_GET_OTHER_REGION_ADDR 0x2
 
-uintptr_t do_sbi_multimem(enclave_id id, uintptr_t call_id);
+uintptr_t do_sbi_multimem(enclave_id id, uintptr_t call_id, uintptr_t arg0);
 
 #endif
