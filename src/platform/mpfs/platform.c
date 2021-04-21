a3346d516d3af9c08468f8a488e25ee8077ee346
Gui Andrade <gh@archshift.com>
2021-04-21 15:01:48 -0700

    mpfs: Output real random data from RNG SBI

--- src/platform/mpfs/platform.c
+++ src/platform/mpfs/platform.c
@@ -1,6 +1,9 @@
 /* Default platform does nothing special here */
-#include "../../enclave.h"
 #include "platform-hook.h"
+#include "../../enclave.h"
+#include "../../../plat/mpfs/drivers/mss_sys_services/mss_sys_services.h"
+#include <sbi/riscv_locks.h>
+#include <assert.h>
 
 unsigned long platform_init_global_once(void){
   return SBI_ERR_SM_ENCLAVE_SUCCESS;
@@ -30,16 +33,27 @@ void platform_switch_from_enclave(struct enclave* enclave){
   return;
 }
 
+#define NONCE_BYTES (256/8)
+#define RAND_STATE_WORDS (NONCE_BYTES/sizeof(uint64_t))
+struct random {
+  uint64_t data[RAND_STATE_WORDS];
+  uint8_t words_left;
+};
+static spinlock_t rand_state_lock = SPIN_LOCK_INITIALIZER;
+static struct random rand_state;
+
 uint64_t platform_random(void){
-#pragma message("Platform has no entropy source, this is unsafe. TEST ONLY")
-  static uint64_t w = 0, s = 0xb5ad4eceda1ce2a9;
+  spin_lock(&rand_state_lock);
 
-  unsigned long cycles;
-  __asm__ __volatile__ ("rdcycle %0" : "=r" (cycles));
+  if (!rand_state.words_left) {
+    // This service provides 256 bits of real random data
+    // It might be a bit slow to query though.
+    int ret = MSS_SYS_nonce_service((uint8_t *)&rand_state.data, 0);
+    sm_assert(ret == 0);
+    rand_state.words_left = RAND_STATE_WORDS;
+  }
+  uint64_t out = rand_state.data[--rand_state.words_left];
 
-  // from Middle Square Weyl Sequence algorithm
-  uint64_t x = cycles;
-  x *= x;
-  x += (w += s);
-  return (x>>32) | (x<<32);
+  spin_unlock(&rand_state_lock);
+  return out;
 }
