663c843d3576c1ea1dba91de4839bea4cd3d205d
Rohit <14986572+Robit@users.noreply.github.com>
2022-04-17 23:05:00 +0000

    bootrom sha256

--- bootrom/string.h
+++ bootrom/string.h
@@ -1,42 +1,13 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
-void* memcpy(void* dest, const void* src, size_t len)
-{
-  const char* s = src;
-  char *d = dest;
+#include <stdint.h>
+#include <stddef.h>
 
-  if ((((uintptr_t)dest | (uintptr_t)src) & (sizeof(uintptr_t)-1)) == 0) {
-    while ((void*)d < (dest + len - (sizeof(uintptr_t)-1))) {
-      *(uintptr_t*)d = *(const uintptr_t*)s;
-      d += sizeof(uintptr_t);
-      s += sizeof(uintptr_t);
-    }
-  }
+void* memcpy(void* dest, const void* src, size_t len);
 
-  while (d < (char*)(dest + len))
-    *d++ = *s++;
+//void* memset(void* dest, int byte, size_t len);
 
-  return dest;
-}
-
-void* memset(void* dest, int byte, size_t len)
-{
-  if ((((uintptr_t)dest | len) & (sizeof(uintptr_t)-1)) == 0) {
-    uintptr_t word = byte & 0xFF;
-    word |= word << 8;
-    word |= word << 16;
-    word |= word << 16 << 16;
-
-    uintptr_t *d = dest;
-    while (d < (uintptr_t*)(dest + len))
-      *d++ = word;
-  } else {
-    char *d = dest;
-    while (d < (char*)(dest + len))
-      *d++ = byte;
-  }
-  return dest;
-}
+void* memset2(void* dest, int byte, size_t len);
 
 #endif
