818da5a6aeee05de35f5ebc838cc8a3d0e0bb7ad
Rohit <14986572+Robit@users.noreply.github.com>
2022-04-25 01:18:50 +0000

    git is silly

--- bootrom/string.h
+++ bootrom/string.h
@@ -2,23 +2,23 @@
 #define __STRING_H__
 
 #include <stdint.h>
-<<<<<<< HEAD
-#include <stddef.h>
-=======
 
 void* memcpy2(void* dest, const void* src, size_t len)
 {
   const char* s = src;
   char *d = dest;
->>>>>>> dev-rohit-sha256-fix
 
-void* memcpy(void* dest, const void* src, size_t len);
+  if ((((uintptr_t)dest | (uintptr_t)src) & (sizeof(uintptr_t)-1)) == 0) {
+    while ((void*)d < (dest + len - (sizeof(uintptr_t)-1))) {
+      *(uintptr_t*)d = *(const uintptr_t*)s;
+      d += sizeof(uintptr_t);
+      s += sizeof(uintptr_t);
+    }
+  }
 
-//void* memset(void* dest, int byte, size_t len);
+  while (d < (char*)(dest + len))
+    *d++ = *s++;
 
-<<<<<<< HEAD
-void* memset2(void* dest, int byte, size_t len);
-=======
   return dest;
 }
 
@@ -40,6 +40,5 @@ void* memset2(void* dest, int byte, size_t len)
   }
   return dest;
 }
->>>>>>> dev-rohit-sha256-fix
 
 #endif
