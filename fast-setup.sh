18b20ba38c3c2e33af97b10d16b38dbfba662af4
Dayeol Lee <dayeol@berkeley.edu>
2022-11-20 22:32:50 +0000

    Fix rv32 test

--- fast-setup.sh
+++ fast-setup.sh
@@ -93,7 +93,7 @@ fi
 GCC_PATH=$(which riscv$BITS-unknown-linux-gnu-gcc)
 RISCV_DIR=$(dirname $(dirname $GCC_PATH))
 echo "export RISCV=$RISCV_DIR" > ./source.sh
-echo "export PATH=$RISCV/bin:\$PATH" >> ./source.sh
+echo "export PATH=\$RISCV/bin:\$PATH" >> ./source.sh
 echo "export KEYSTONE_SDK_DIR=$KEYSTONE_SDK_DIR" >> ./source.sh
 
 echo "RISC-V toolchain and Keystone SDK have been fully setup"
