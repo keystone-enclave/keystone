7c9140014587e89d5b982d9b6790d41c269a6806
Gregor Haas <ghaas97@cs.washington.edu>
2023-01-17 16:23:12 -0800

    Expand syscall space

--- runtime/call/sbi.c
+++ runtime/call/sbi.c
@@ -2,27 +2,38 @@
 
 #include "mm/vm_defs.h"
 
-#define SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2)    \
+#define SBI_CALL(___ext, ___which, ___arg0, ___arg1,             \
+                 ___arg2, ___arg3, ___arg4, ___arg5)             \
   ({                                                             \
     register uintptr_t a0 __asm__("a0") = (uintptr_t)(___arg0);  \
     register uintptr_t a1 __asm__("a1") = (uintptr_t)(___arg1);  \
     register uintptr_t a2 __asm__("a2") = (uintptr_t)(___arg2);  \
+    register uintptr_t a3 __asm__("a3") = (uintptr_t)(___arg3);  \
+    register uintptr_t a4 __asm__("a4") = (uintptr_t)(___arg4);  \
+    register uintptr_t a5 __asm__("a5") = (uintptr_t)(___arg5);  \
     register uintptr_t a6 __asm__("a6") = (uintptr_t)(___which); \
     register uintptr_t a7 __asm__("a7") = (uintptr_t)(___ext);   \
     __asm__ volatile("ecall"                                     \
                      : "+r"(a0)                                  \
-                     : "r"(a1), "r"(a2), "r"(a6), "r"(a7)        \
+                     : "r"(a1), "r"(a2), "r"(a3), "r"(a4),       \
+                       "r"(a5), "r"(a6), "r"(a7)                 \
                      : "memory");                                \
     a0;                                                          \
   })
 
 /* Lazy implementations until SBI is finalized */
-#define SBI_CALL_0(___ext, ___which) SBI_CALL(___ext, ___which, 0, 0, 0)
-#define SBI_CALL_1(___ext, ___which, ___arg0) SBI_CALL(___ext, ___which, ___arg0, 0, 0)
+#define SBI_CALL_0(___ext, ___which) SBI_CALL(___ext, ___which, 0, 0, 0, 0, 0, 0)
+#define SBI_CALL_1(___ext, ___which, ___arg0) SBI_CALL(___ext, ___which, ___arg0, 0, 0, 0, 0, 0)
 #define SBI_CALL_2(___ext, ___which, ___arg0, ___arg1) \
-  SBI_CALL(___ext, ___which, ___arg0, ___arg1, 0)
+  SBI_CALL(___ext, ___which, ___arg0, ___arg1, 0, 0, 0, 0)
 #define SBI_CALL_3(___ext, ___which, ___arg0, ___arg1, ___arg2) \
-  SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2)
+  SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2, 0, 0, 0)
+#define SBI_CALL_4(___ext, ___which, ___arg0, ___arg1, ___arg2, ___arg3) \
+  SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2, ___arg3, 0, 0)
+#define SBI_CALL_5(___ext, ___which, ___arg0, ___arg1, ___arg2, ___arg3, ___arg4) \
+  SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2, ___arg3, __arg4, 0)
+#define SBI_CALL_6(___ext, ___which, ___arg0, ___arg1, ___arg2, ___arg3, ___arg4, ___arg5) \
+  SBI_CALL(___ext, ___which, ___arg0, ___arg1, ___arg2, ___arg3, ___arg4, ___arg5)
 
 void
 sbi_putchar(char character) {
--- sdk/include/app/syscall.h
+++ sdk/include/app/syscall.h
@@ -11,30 +11,34 @@
 
 #include "shared/eyrie_call.h"
 
-#define SYSCALL(which, arg0, arg1, arg2, arg3, arg4)           \
+#define SYSCALL(which, arg0, arg1, arg2, arg3, arg4, arg5)     \
   ({                                                           \
     register uintptr_t a0 asm("a0") = (uintptr_t)(arg0);       \
     register uintptr_t a1 asm("a1") = (uintptr_t)(arg1);       \
     register uintptr_t a2 asm("a2") = (uintptr_t)(arg2);       \
     register uintptr_t a3 asm("a3") = (uintptr_t)(arg3);       \
     register uintptr_t a4 asm("a4") = (uintptr_t)(arg4);       \
+    register uintptr_t a5 asm("a5") = (uintptr_t)(arg5);       \
     register uintptr_t a7 asm("a7") = (uintptr_t)(which);      \
     asm volatile("ecall"                                       \
                  : "+r"(a0)                                    \
-                 : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a7) \
+                 : "r"(a1), "r"(a2), "r"(a3), "r"(a4),         \
+                   "r"(a5), "r"(a7)                            \
                  : "memory");                                  \
     a0;                                                        \
   })
 
-#define SYSCALL_0(which) SYSCALL(which, 0, 0, 0, 0, 0)
-#define SYSCALL_1(which, arg0) SYSCALL(which, arg0, 0, 0, 0, 0)
-#define SYSCALL_2(which, arg0, arg1) SYSCALL(which, arg0, arg1, 0, 0, 0)
+#define SYSCALL_0(which) SYSCALL(which, 0, 0, 0, 0, 0, 0)
+#define SYSCALL_1(which, arg0) SYSCALL(which, arg0, 0, 0, 0, 0, 0)
+#define SYSCALL_2(which, arg0, arg1) SYSCALL(which, arg0, arg1, 0, 0, 0, 0)
 #define SYSCALL_3(which, arg0, arg1, arg2) \
-  SYSCALL(which, arg0, arg1, arg2, 0, 0)
+  SYSCALL(which, arg0, arg1, arg2, 0, 0, 0)
 #define SYSCALL_4(which, arg0, arg1, arg2, arg3) \
-  SYSCALL(which, arg0, arg1, arg2, arg3, 0)
+  SYSCALL(which, arg0, arg1, arg2, arg3, 0, 0)
 #define SYSCALL_5(which, arg0, arg1, arg2, arg3, arg4) \
-  SYSCALL(which, arg0, arg1, arg2, arg3, arg4)
+  SYSCALL(which, arg0, arg1, arg2, arg3, arg4, 0)
+#define SYSCALL_6(which, arg0, arg1, arg2, arg3, arg4, arg5) \
+  SYSCALL(which, arg0, arg1, arg2, arg3, arg4, arg5)
 
 int
 copy_from_shared(void* dst, uintptr_t offset, size_t data_len);
