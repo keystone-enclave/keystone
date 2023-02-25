4b219da471b68beeab8d1765ca498443d21c18a2
red-robby <jakobsorensen@berkeley.edu>
2023-02-25 15:54:37 -0800

    Refactor script to remove redundant max attempt check

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,30 +10,22 @@ screen -L -dmS qemu ./scripts/run-qemu.sh
 sleep 1
 
 # Verify VM is running before running tests - abort after 10 failed attempts
-running=0
-for ((i = 0; i < $ATTEMPTS; ++i)); do
+for ((i = 0;;)); do
     result=$((echo "info status" && sleep 1) | \
 	    socat - unix-connect:qemu-monitor.sock | \
 	    grep -oF "running")
-    if [[ -z "$result" ]]; then
-	attempt=$((i+1))
-	echo "VM not yet running. (check $attempt/$ATTEMPTS)"
-	[ $attempt -ne $ATTEMPTS ] && sleep $ATTEMPT_DELAY
-    else
-	echo "VM confirmed to be running. Proceeding to run test(s)."
-	running=1
-	break
+    [[ -z "$result" ]] || break
+    echo "VM not yet running. (check $((++i))/$ATTEMPTS)"
+    if [[ $i -eq $ATTEMPTS ]]; then
+	echo "[FAIL] VM not confirmed as running after max of $ATTEMPTS checks - aborting."
+	exit 1
     fi
+    sleep $ATTEMPT_DELAY
 done
 
 # Clean up socket
 rm $MONITOR_SOCKET
-
-if [ $running -eq 0 ]; then
-    echo "[FAIL] VM not confirmed as running after max of $ATTEMPTS attempts - aborting."
-    exit 1
-fi
-
+echo "VM confirmed to be running. Proceeding to run test(s)."
 
 ./scripts/test-qemu.sh 2>&1 | grep -v "Warning: Permanently added" | tee output.log
 
