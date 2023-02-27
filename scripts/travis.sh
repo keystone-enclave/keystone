5dc036c8958325c17911cc276da51d7c9fc1d21b
red-robby <jakobsorensen@berkeley.edu>
2023-02-26 16:34:05 -0800

    Use netcat instead of socat

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,7 +12,7 @@ sleep 1
 # Verify VM is running before running tests - abort after 10 failed attempts
 for ((i = 0;;)); do
     result=$( (echo "info status" && sleep 1) | \
-	    socat - unix-connect:$MONITOR_SOCKET | \
+	    nc -U $MONITOR_SOCKET -w 2 | \
 	    grep -oF "running")
     [[ -z "$result" ]] || break
     echo "VM not yet running. (check $((++i))/$ATTEMPTS)"
