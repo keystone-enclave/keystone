ac6cb5030208c1b84a52b19485896846d3806566
Gregor Haas <ghaas97@cs.washington.edu>
2023-06-20 13:43:10 -0700

    Build SM in Buildroot

---
+++ sm/src/objects.mk
@@ -0,0 +1,42 @@
+#############
+## Headers ##
+#############
+
+# General headers
+keystone-sm-headers += assert.h cpu.h enclave.h ipi.h mprv.h page.h platform-hook.h \
+                        pmp.h safe_math_util.h sm.h sm-sbi.h sm-sbi-opensbi.h thread.h
+
+# Crypto headers
+keystone-sm-headers += crypto.h ed25519/ed25519.h ed25519/fe.h ed25519/fixedint.h \
+                        ed25519/ge.h ed25519/precomp_data.h ed25519/sc.h \
+                        hkdf_sha3_512/hkdf_sha3_512.h hmac_sha3/hmac_sha3.h \
+                        sha3/sha3.h
+
+# Platform headers
+keystone-sm-headers += platform/$(KEYSTONE_PLATFORM)/platform.h
+
+ifeq ($(KEYSTONE_PLATFORM),sifive/fu540)
+	keystone-sm-headers += platform/sifive/fu540/waymasks.h
+endif
+
+# Plugin headers
+keystone-sm-headers += plugins/multimem.h plugins/plugins.h
+
+##################
+## Source files ##
+##################
+
+# Core files
+keystone-sm-sources += attest.c cpu.c enclave.c pmp.c sm.c sm-sbi.c sm-sbi-opensbi.c \
+                        thread.c mprv.c sbi_trap_hack.c trap.c ipi.c
+
+# Crypto
+keystone-sm-sources += crypto.c sha3/sha3.c ed25519/fe.c ed25519/ge.c ed25519/keypair.c \
+                        ed25519/sc.c ed25519/sign.c hkdf_sha3_512/hkdf_sha3_512.c \
+                        hmac_sha3/hmac_sha3.c
+
+# Platform
+keystone-sm-sources += platform/$(PLATFORM)/platform.c
+
+# Plugin files
+keystone-sm-sources += plugins/multimem.c plugins/plugins.c
