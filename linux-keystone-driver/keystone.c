f18fc7be27e05382e3084b2497e3bb795eecad54
Gregor Haas <ghaas97@cs.washington.edu>
2023-08-15 15:42:33 -0700

    Force usage of CMA on MPFS platform

--- linux-keystone-driver/keystone.c
+++ linux-keystone-driver/keystone.c
@@ -86,7 +86,12 @@ static int __init keystone_dev_init(void)
     pr_err("keystone_enclave: misc_register() failed\n");
   }
 
+#ifdef KEYSTONE_PLAT_mpfs
+  keystone_dev.this_device->dma_coherent = true;
+  keystone_dev.this_device->coherent_dma_mask = DMA_BIT_MASK(64);
+#else
   keystone_dev.this_device->coherent_dma_mask = DMA_BIT_MASK(32);
+#endif
 
   pr_info("keystone_enclave: " DRV_DESCRIPTION " v" DRV_VERSION "\n");
   return ret;
