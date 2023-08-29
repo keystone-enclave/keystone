b48e520fa27a09ad307b996297884df5fed54ca5
Gregor Haas <ghaas97@cs.washington.edu>
2023-08-29 14:49:34 -0700

    Add packages for the runtime and the SM

---
+++ overlays/keystone/package/keystone-runtime/keystone-runtime.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# Runtime
+#
+################################################################################
+
+ifeq ($(KEYSTONE_RUNTIME),)
+$(error KEYSTONE_RUNTIME directory not defined)
+else
+include $(KEYSTONE)/mkutils/pkg-keystone.mk
+endif
+
+# This is a bit of a dummy package, we only want it to copy its sources so that
+# we can use it in the examples. Also, for change tracking as described in
+# pkg-keystone.mk
+
+# Clean the examples too if we clean this package
+keystone-runtime-dirclean: keystone-examples-dirclean
+
+$(eval $(keystone-package))
+$(eval $(generic-package))
