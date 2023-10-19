27883e99de5e609e7124c7937ca3429f2d48e299
Evgeny P <evgenyp@berkeley.edu>
2023-10-18 22:11:51 -0700

    updated instructions

--- fast-setup.sh
+++ fast-setup.sh
@@ -1,21 +1,10 @@
-export TOOLCHAIN_7Z_FILE=riscv-toolchain-lp64d-rv64gc-2021.01.bionic.7z
-wget https://keystone-enclave.eecs.berkeley.edu/files/$TOOLCHAIN_7Z_FILE
-7za x -y $TOOLCHAIN_7Z_FILE -o./old-toolchain
-rm $TOOLCHAIN_7Z_FILE
+git submodule update --init --recursive --depth 1
+echo "please read this file for further instructions"
 
-export RISCV=$(pwd)/old-toolchain
-export PATH=$PATH:$RISCV/bin
-echo "export RISCV=$RISCV_DIR" > ./source.sh
-echo "export PATH=\$RISCV/bin:\$PATH" >> ./source.sh
-
-echo "Remember to source ./source.sh !!!"
-
-# INSTRUCTIONS for running
-# ./fast-setup.sh
-# source ./source.sh
-# git submodule update --init --recursive --depth 1
-# make -j 12 # more if available
+### INSTRUCTIONS
+# make -j 12 # change 12 to desired parallelism
+### find port in command from make # 9821
 # make run
-# # find port in command from make # 9821
+### switch terminals
 # scp -i build-generic64/overlay/root/.ssh/id-rsa -P 9821 build-generic64/buildroot.build/build/keystone-*/*.ko root@localhost:.
 # scp -i build-generic64/overlay/root/.ssh/id-rsa -P 9821 build-generic64/buildroot.build/build/keystone-examples-*/*/*.ke root@localhost:.
