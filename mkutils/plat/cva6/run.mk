b69aae01bebf19fd3bdc912a264890331450ac5c
sahmad <syarif.ahmad@thalesgroup.com>
2023-10-26 15:37:36 +0800

    fix the flash script

--- mkutils/plat/cva6/run.mk
+++ mkutils/plat/cva6/run.mk
@@ -2,20 +2,22 @@
 ## CVA6 Flash SD Card ##
 ######################
 
-ifndef SD_DEVICE
-$(error SD_DEVICE is not set)
-endif
 
 
-PAYLOAD                 := $(BUILDROOT_BUILDDIR)/images/fw_payload.bin
-PAYLOAD_SECTOR_START    := 2048
-PAYLOAD_SECTOR_START    := $(ls -l --block-size=1 $PAYLOAD | cut -d " " -f5)
-PAYLOAD_SECTOR_END      := $(echo $PAYLOAD_SECTOR_START+$PAYLOAD_SECTOR_SIZE | bc)
-SDDEVICE_PART1          :=${SDDEVICE}1
-SDDEVICE_PART2          :=${SDDEVICE}2
-
-flash:
-	sgdisk --clear -g --new=1:$(PAYLOAD_SECTOR_START):30M --new=2:512M:0 --typecode=1:3000 --typecode=2:8300 $(SD_DEVICE)
-        dd if=$PAYLOAD of=$SDDEVICE_PART1 status=progress oflag=sync bs=1M
+PAYLOAD                 = $(BUILDROOT_BUILDDIR)/images/fw_payload.bin
+SDDEVICE_PART1          = $(SD_DEVICE)1
+SDDEVICE_PART2          = $(SD_DEVICE)2
 
 
+
+flash: $(SD_DEVICE)
+	ifndef SD_DEVICE
+		$(error SD_DEVICE is not set)
+	endif
+	$(info PAYLOAD INFORMATION)
+	$(info $(PAYLOAD))
+	$(info $(SD_DEVICE))
+	$(info $(SDDEVICE_PART1))
+	$(info $(SDDEVICE_PART2))
+	sgdisk --clear -g --new=1:2048:30M --new=2:512M:0 --typecode=1:3000 --typecode=2:8300 $(SD_DEVICE)
+	dd if=$(PAYLOAD) of=$(SDDEVICE_PART1) status=progress oflag=sync bs=1M
