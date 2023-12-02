a3a4865393bcc694bbf480581f031c690af2beec
Evgeny P <evgenyp@berkeley.edu>
2023-12-01 18:47:18 -0800

    PR comments addressed

--- fast-setup.sh
+++ fast-setup.sh
@@ -3,8 +3,8 @@ echo "please read this file for further instructions"
 
 ### INSTRUCTIONS
 # make -j 12 # change 12 to desired parallelism
-### find port in command from make # 9821
+### find port in command from make, 9821 at the time of writing
 # make run
 ### switch terminals
-# scp -i build-generic64/overlay/root/.ssh/id-rsa -P 9821 build-generic64/buildroot.build/build/keystone-*/*.ko root@localhost:.
-# scp -i build-generic64/overlay/root/.ssh/id-rsa -P 9821 build-generic64/buildroot.build/build/keystone-examples-*/*/*.ke root@localhost:.
+# scp -i build-generic64/overlay/root/.ssh/id-rsa -P <PORT_NUMBER> build-generic64/buildroot.build/build/keystone-*/*.ko root@localhost:.
+# scp -i build-generic64/overlay/root/.ssh/id-rsa -P <PORT_NUMBER> build-generic64/buildroot.build/build/keystone-examples-*/*/*.ke root@localhost:.
