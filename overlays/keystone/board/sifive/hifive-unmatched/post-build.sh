dc22b1b291c8204e7f7d2da59553d817b6cbac2d
Akihiro Saiki <saiki@kasahara.cs.waseda.ac.jp>
2023-12-04 11:43:17 +0900

    Remove GPL licensed code
    Some files imported from freedom-u-sdk are GPL licensed.
    So they're removed from keystone repo and directly downloaded at build-time.

--- overlays/keystone/board/sifive/hifive-unmatched/post-build.sh
+++ overlays/keystone/board/sifive/hifive-unmatched/post-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+FUSDK_VER="2023.08.00"
+
 # Copy extlinux config
 mkdir -p $TARGET_DIR/boot/extlinux
 cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
@@ -6,6 +9,6 @@ cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/extlinux.conf $TARG
 # Install udev rules & systemd units
 mkdir -p $TARGET_DIR/usr/lib/systemd/system
 mkdir -p $TARGET_DIR/etc/udev/rules.d
-cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/unmatched-systemd-units/*.timer $TARGET_DIR/usr/lib/systemd/system/
-cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/unmatched-systemd-units/*.service $TARGET_DIR/usr/lib/systemd/system/
-cp $BR2_EXTERNAL_KEYSTONE_PATH/board/sifive/hifive-unmatched/unmatched-udev-rules/*.rules $TARGET_DIR/etc/udev/rules.d/
+wget -P "${TARGET_DIR}/etc/udev/rules.d" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-udev-rules/files/unmatched/99-pwm-leds.rules"
+wget -P "${TARGET_DIR}/usr/lib/systemd/system" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-systemd-units/files/led-bootstate-green.service"
+wget -P "${TARGET_DIR}/usr/lib/systemd/system" "https://raw.githubusercontent.com/sifive/freedom-u-sdk/${FUSDK_VER}/recipes-sifive/unmatched-systemd-units/files/led-bootstate-green.timer"
