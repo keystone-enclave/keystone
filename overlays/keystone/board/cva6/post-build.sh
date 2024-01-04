1d6851820851b75a08cac2def16c8164c97ffd3a
asyarifstudio <syarif.ahmad@thalesgroup.com>
2024-01-04 12:43:01 +0800

    fix the post build recursive argument

--- overlays/keystone/board/cva6/post-build.sh
+++ overlays/keystone/board/cva6/post-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp $BR2_EXTERNAL_KEYSTONE_PATH/board/cva6/cva6-sdk/rootfs/* $BUILDROOT_OVERLAYDIR/
+cp -rf $BR2_EXTERNAL_KEYSTONE_PATH/board/cva6/cva6-sdk/rootfs/* $BUILDROOT_OVERLAYDIR/ | true
 
 # Install udev rules & systemd units
